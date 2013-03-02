94e4c599418d396d64ba1304f801c7632357c245
Bjorn Vaardal <bjorn@vaardal.com>
2013-03-02 15:31:49 -0500

    Ported to WPF. Dynamic lights. Proper async sound.

---
+++ WpfApplication1/Components/Beat.cs
@@ -0,0 +1,19 @@
+﻿namespace MetronomeWPF.Components
+{
+    class Beat
+    {
+        public BeatState BeatState { get; set; }
+
+        public Beat(BeatState beatState)
+        {
+            BeatState = beatState;
+        }
+    }
+
+    enum BeatState
+    {
+        On,
+        Off,
+        Emphasized
+    }
+}
---
+++ WpfApplication1/Components/Metronome.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace MetronomeWPF.Components
+{
+    class Metronome
+    {
+        public int Tempo { get; private set; }
+        public Beat[] beats { get; private set; }
+        private TimeSignature TimeSignature;
+
+        Timer trigger;
+        private int currentBeat;
+        private Mutex currentBeatMutex;
+
+        public Metronome()
+            : this(new TimeSignature(4, 4), 120) { }
+
+        public Metronome(TimeSignature timeSignature, int tempo)
+        {
+            this.ChangeTimeSignature(timeSignature);
+            if (tempo > 0)
+            {
+                Tempo = tempo;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("tempo", "Tempo cannot be negative");
+            }
+        }
+
+        /**
+         *  <summary>
+         *      Created sound threads at given interval.
+         *  </summary>
+         */
+        public void StartMetronome(Action<Beat> tick)
+        {
+            currentBeat = 0;
+            currentBeatMutex = new Mutex(false);
+            
+            trigger = new Timer(
+                (source) => 
+                {
+                    // Get beat number (thread-safe)
+                    currentBeatMutex.WaitOne();
+                    int beat = currentBeat++;
+                    currentBeat %= beats.Length;
+                    currentBeatMutex.ReleaseMutex();
+
+                    // Get beat info and set sound
+                    Beat b = beats[beat];
+                    tick(b);
+                },
+                null,Timeout.InfiniteTimeSpan,Timeout.InfiniteTimeSpan);
+            trigger.Change(TimeSpan.Zero, TimeSpan.FromMilliseconds(60000 / Tempo));
+        }
+
+        /**
+         *  <summary>
+         *      Stop sound thread creation.
+         *  </summary>
+         */
+        public void StopMetronome()
+        {
+            trigger.Change(Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan);
+        }
+
+        public void ChangeTimeSignature(TimeSignature newTimeSignature)
+        {
+            this.TimeSignature = newTimeSignature;
+
+            beats = new Beat[this.TimeSignature.BeatsPerBar];
+            for (int i = 0; i < beats.Length; i++)
+            {
+                beats[i] = new Beat(BeatState.On);
+            }
+            beats[0].BeatState = BeatState.Emphasized;
+        }
+
+        public void ChangeBeatState(int beat, BeatState state)
+        {
+            beats[beat].BeatState = state;
+        }
+    }
+}
