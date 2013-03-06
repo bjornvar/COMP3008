66eb8ee908816119f572a6b4cb8fd548ac555a43
Chris Cowan <ch.cowan@gmail.com>
2013-03-05 20:58:24 -0500

    Tempo from taps is applied to metronome
    The registered taps from the tap screen can now be applied to the
    metronome by clicking the checkmark button. This includes a check for
    values outside of the range of 20 - 240.

--- MetronomeWPF/Components/TimeSignature.cs
+++ MetronomeWPF/Components/TimeSignature.cs
@@ -1,6 +1,6 @@
 ﻿namespace MetronomeWPF.Components
 {
-    class TimeSignature
+    public class TimeSignature
     {
         public int BeatsPerBar { get; private set; }
         public int BeatLength { get; private set; }
